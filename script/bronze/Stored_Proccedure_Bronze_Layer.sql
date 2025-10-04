9831de264a13b42c8414937552bad8608f0c21df
AHMED88899 <118996261+AHMED88899@users.noreply.github.com>
2025-10-04 20:59:43 +0300

    Create Stored_Proccedure_Bronze_Layer.sql

---
+++ script/bronze/Stored_Proccedure_Bronze_Layer.sql
@@ -0,0 +1,161 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+
+
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME,@end_time DATETIME,@batch_start_time DATETIME,@batch_end_time DATETIME
+	BEGIN TRY
+		PRINT'===============================================';
+		PRINT'Loadinh Bronze Layer';
+		PRINT'===============================================';
+
+
+		PRINT'----------------------------------------';
+		PRINT'Loaading CRM Tables';
+		PRINT'----------------------------------------';
+		SET @batch_start_time= GETDATE()
+
+
+		PRINT'>> Truncating Table : bronze.crm_cust_info';
+		SET @start_time=GETDATE();
+		TRUNCATE TABLE [bronze].[crm._cust_info]
+
+		PRINT'>> Bulk Insert Table : bronze.crm_cust_info';
+		BULK INSERT [bronze].[crm._cust_info]
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH(
+			FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' +' '+ CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+		SET @start_time=GETDATE();
+		TRUNCATE TABLE bronze.crm_prd_info
+
+		PRINT'>> Bulk Insert Table : bronze.crm_prd_info';
+
+		BULK INSERT bronze.crm_prd_info
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH(
+			FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' + CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+		PRINT'>> Truncating Table : bronze.crm_sales_info';
+		SET @start_time=GETDATE();
+		TRUNCATE TABLE bronze.crm_sales_details
+		PRINT'>> Bulk Insert Table : bronze.crm_sales_info';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH(
+			FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' + CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+		PRINT'----------------------------------------';
+		PRINT'Loaading CRM Tables';
+		PRINT'----------------------------------------';
+
+
+		PRINT'>> Truncating Table : bronze.erp_loc_a101';
+		SET @start_time=GETDATE();
+		TRUNCATE TABLE bronze.erp_loc_a101
+		PRINT'>> Bulk Insert Table : bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH(
+			FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' + CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+		PRINT'>> Truncating Table : bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12
+		SET @start_time=GETDATE();
+		PRINT'>> Bulk Insert Table : bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH(	FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' + CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+
+		PRINT'>> Truncating Table : bronze.erp_px_cat_g1v2';
+		SET @start_time=GETDATE();
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2
+		PRINT'>> Bulk Insert Table : bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'F:\DW Projects\DW Bara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH(
+			FIRSTROW =2,
+			FIELDTERMINATOR =',',
+			TABLOCK
+		)
+
+		SET @end_time=GETDATE();
+		PRINT'>> Load Duration' + CAST(DATEDIFF(SECOND, @start_time, @end_time)AS NVARCHAR)+' ' + 'seoneds'
+		PRINT'>> Truncating Table : bronze.crm_prd_info';
+		PRINT'>> -------------------------------------'
+
+		PRINT '>>====================================================================='
+		SET @batch_end_time = GETDATE()
+		PRINT'============================================================='
+		PRINT'Loading bronzelayer is compleated'
+		PRINT'total loadinding duration is :'+ ' '+ cast(datediff(second,@batch_start_time,@batch_end_time) AS nvarchar)+' '+'second'
+		PRINT'++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+	END TRY
+	BEGIN CATCH
+		PRINT'========================================================';
+		print'error happended during loading bronze layer';
+		print'error massage'+ ERROR_MESSAGE();
+		print'error massage'+ CAST(ERROR_NUMBER() AS NVARCHAR)
+		print'error massage'+ CAST(ERROR_STATE() AS NVARCHAR)
+
+
+		PRINT'========================================================'
+
+	END CATCH
+END 
+
